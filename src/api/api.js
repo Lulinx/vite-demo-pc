d626c3e91a4de2f843d77c9f6e3c22730a30f80b
Lulinx <Mr_Wangs@163.com>
2022-01-17 15:00:25 +0800

    注释

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,19 @@
+/*
+ * @Author: ws
+ * @Date: 2022-01-17 08:56:49
+ * @LastEditTime: 2022-01-17 14:35:21
+ * @LastEditors: ws
+ * @Description: 用于存放工程内所有的接口API方法
+ * @FilePath: \myDemo\vite-demo-pc\src\api\api.js
+ */
 import request from "root/request/request";
 import mockData from "root/mock/mock";
 const Mock = true;
 const baseUrl =
   process.env.NODE_ENV === "production" ? "https://api.apishop.net" : "";
+
+
+  
 // 狗狗大全
 export const dogFamily = (data = {}, callback = "") =>
   Mock
